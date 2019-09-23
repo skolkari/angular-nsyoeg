d53c912841333400235ac69a40a3886c1ad35f80
Salma Kolkari <34543416+skolkari@users.noreply.github.com>
2019-09-23 23:20:36 +0530

    latest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+
+  title = 'angular5-seed';
+  hideDefaultHeader = false;
+
+  constructor(private appService: AppService) {
+   
+    this.appService.headerObservable.subscribe(data => {
+      this.hideDefaultHeader = data;
+    });
+  }
+
+  ngOnInit() {
+    console.log('hideDefaultHeader 34 => ', this.hideDefaultHeader);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SampleOneComponent } from './sample-one/sample-one.component';
+import { SampleTwoComponent } from './sample-two/sample-two.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
+import { AppService } from './app.service';
+import { HomeComponent } from './home/home.component';
+import { SampleThreeComponent } from './sample-three/sample-three.component';
+import { Router } from '@angular/router';
+import { SampleFourComponent } from './sample-four/sample-four.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    SampleOneComponent,
+    SampleTwoComponent,
+    SampleThreeComponent,
+    PageNotFoundComponent,
+    NavHeaderComponent,
+    NavFooterComponent,
+    HomeComponent,
+    SampleFourComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
